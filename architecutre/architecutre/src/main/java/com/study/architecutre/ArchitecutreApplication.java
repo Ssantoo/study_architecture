91a111494b836b149c1cdea24373bffba30a9f6a
HyunjaeCho <nowing0108@kcinfra.co.kr>
2024-07-09 15:50:34 +0900

    init. project setting

---
+++ architecutre/architecutre/src/main/java/com/study/architecutre/ArchitecutreApplication.java
@@ -0,0 +1,13 @@
+package com.study.architecutre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecutreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitecutreApplication.class, args);
+	}
+
+}
---
+++ architecutre/architecutre/src/test/java/com/study/architecutre/ArchitecutreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.architecutre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecutreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
